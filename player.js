f2651d43cf6d2ba21b5a5312af2b3bfe9f095101
cal-hogbenSBHS <64843626+cal-hogbenSBHS@users.noreply.github.com>
2021-08-04 10:42:14 +1200

    Add files via upload

--- player.js
+++ player.js
@@ -1,5 +1,6 @@
 class Player {
-  constructor(x, y, w, h, c, xSpeed, ySpeed) {
+  constructor(image,x, y, w, h, c, xSpeed, ySpeed) {
+    this.image = image
     this.x = x;
     this.y = y;
     this.w = w;
@@ -14,25 +15,23 @@ class Player {
     canvasContext.fillRect(this.x, this.y, this.w, this.h);
   }
 
+  drawimgs(){
+    canvasContext.drawImage(this.image, this.x, this.y, this.w, this.h);
+  }
+
   playerMove() {
-    
-    if(rightPressed) {
+
+    if (rightPressed) {
       this.x += this.xSpeed;
-      if(this.x > canvas.width){ // once out of right screen wrap to left 
+      if (this.x > canvas.width) { // once out of right screen wrap to left 
         this.x = 0;
       }
     }
-    if(leftPressed) {
+    if (leftPressed) {
       this.x -= this.xSpeed;
-      if(this.x < 0){ // once out of left side of screen wrap to right
+      if (this.x < 0) { // once out of left side of screen wrap to right
         this.x = canvas.width - this.w;
       }
-    }
-    if(upPressed && this.y > 0){ // cant go out of top of screen
-      this.y -= this.ySpeed;
-    }
-    if(downPressed && this.y + this.h < canvas.height){ // cant go out of bottom of screen
-      this.y += this.ySpeed;
     }
   }
 
