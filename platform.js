5ff4e48b23e8a1f2e976c249d009ef1aee9cf866
cal-hogbenSBHS <64843626+cal-hogbenSBHS@users.noreply.github.com>
2021-09-22 11:22:47 +1200

    Add files via upload

---
+++ platform.js
@@ -0,0 +1,26 @@
+ class Platform {
+   constructor(x, y, w, h, c, xSpeed) {
+     //   this.image = image
+     this.x = x;
+     this.y = y;
+     this.w = w;
+     this.h = h;
+     this.c = c;
+     this.xSpeed = xSpeed;
+
+   }
+
+   drawPlat() {
+     canvasContext.fillStyle = this.c;
+     canvasContext.fillRect(this.x, this.y, this.w, this.h);
+   }
+
+   movePlat(){
+    this.x -= this.xSpeed;
+
+  if(this.x < 0){
+    this.x = canvas.width;
+  }
+   }
+
+ }
--- player.js
+++ player.js
@@ -1,5 +1,5 @@
 class Player {
-  constructor(image,x, y, w, h, c, xSpeed, ySpeed) {
+  constructor(image, x, y, w, h, c, xSpeed, yVel, rate, imgMode) {
     this.image = image
     this.x = x;
     this.y = y;
@@ -7,19 +7,41 @@ class Player {
     this.h = h;
     this.c = c;
     this.xSpeed = xSpeed;
-    this.ySpeed = ySpeed;
+    this.yVel = yVel;
+    this.rate = rate;
+    this.imgMode = imgMode;
   }
 
-  drawRect() {
-    canvasContext.fillStyle = this.c;
-    canvasContext.fillRect(this.x, this.y, this.w, this.h);
-  }
-
-  drawimgs(){
+  drawimgs() {
     canvasContext.drawImage(this.image, this.x, this.y, this.w, this.h);
   }
 
+  changeGravity() {
+    console.log(this.rate);
+    holding = true;
+    if (this.rate > 0) {
+      this.yVel = 1; // if postive or negative this.rate = 1 or -1
+      this.imgMode = 1;
+      console.log(this.imgMode)
+    } else {
+      this.yVel = -1; // if postive or negative this.rate = 1 or -1
+      this.imgMode = 0;
+      console.log(this.imgMode)
+    }
+    this.rate *= -1;
+  }
+
   playerMove() {
+    this.y += this.yVel; //Gravity
+    this.yVel += this.rate;
+
+    if (this.y > canvas.height) {
+      this.y = 0
+    }
+
+    if (this.y < 0) {
+      this.y = canvas.height - this.h;
+    }
 
     if (rightPressed) {
       this.x += this.xSpeed;
@@ -27,13 +49,15 @@ class Player {
         this.x = 0;
       }
     }
+
     if (leftPressed) {
       this.x -= this.xSpeed;
       if (this.x < 0) { // once out of left side of screen wrap to right
         this.x = canvas.width - this.w;
       }
     }
+
+
+
   }
-
-
 }
